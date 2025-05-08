956bb26bbc9f69e93cf597859063c388cdf0b9c7
AdeptEagle <kerbi2600@gmail.com>
2025-05-09 07:22:55 +0800

    changes for online

--- _helpers/db.js
+++ _helpers/db.js
@@ -1,53 +1,57 @@
-const config = require('../config.json')
-const mysql = require('mysql2/promise')
+const config = require(process.env.NODE_ENV === 'production' ? '../config.prod.json' : '../config.json')
 const { Sequelize } = require('sequelize')
 
 module.exports = db = { }
 
 async function initialize(){
-  const { host, port, user, password, database } = config.database
-  const connection = await mysql.createConnection({ 
-    host, 
-    port, 
-    user, 
-    password,
-    database  // Add database name here
-  })
+  const { host, port, user, password, database, dialect } = config.database
+  console.log('Connecting to database:', { host, port, user, database, dialect })
+  
+  try {
+    const sequelize = new Sequelize(database, user, password, { 
+      host,
+      port,
+      dialect: dialect || 'mysql',
+      logging: false // Set to console.log to see SQL queries
+    })
 
-  const sequelize = new Sequelize(database, user, password, { 
-    host,
-    port,
-    dialect: 'mysql'
-  })
+    // Test the connection
+    await sequelize.authenticate()
+    console.log('Database connection successful')
 
-  db.Account = require('../accounts/account.model')(sequelize)
-  db.Department = require('../departments/department.model')(sequelize)
-  db.refreshToken = require('../accounts/refresh-token.model')(sequelize)
-  db.Employee = require('../employees/employee.model')(sequelize)
+    db.Account = require('../accounts/account.model')(sequelize)
+    db.Department = require('../departments/department.model')(sequelize)
+    db.refreshToken = require('../accounts/refresh-token.model')(sequelize)
+    db.Employee = require('../employees/employee.model')(sequelize)
 
-  // one to many (account -> refreshToken)
-  db.Account.hasMany(db.refreshToken, { onDelete: 'CASCADE' })
-  db.refreshToken.belongsTo(db.Account)
+    // one to many (account -> refreshToken)
+    db.Account.hasMany(db.refreshToken, { onDelete: 'CASCADE' })
+    db.refreshToken.belongsTo(db.Account)
 
-  // one to one (employee to account)
-  db.Account.hasOne(db.Employee, {
-    foreignKey: 'userId',
-    onDelete: 'CASCADE'
-  })
-  db.Employee.belongsTo(db.Account, {
-    foreignKey: 'userId'
-  })  
+    // one to one (employee to account)
+    db.Account.hasOne(db.Employee, {
+      foreignKey: 'userId',
+      onDelete: 'CASCADE'
+    })
+    db.Employee.belongsTo(db.Account, {
+      foreignKey: 'userId'
+    })  
 
-  // one to many (employee -> department) 
-  db.Department.hasMany(db.Employee, {
-    foreignKey: 'departmentId',
-    onDelete: 'SET NULL'
-  })  
-  db.Employee.belongsTo(db.Department, {
-    foreignKey: 'departmentId'
-  })
+    // one to many (employee -> department) 
+    db.Department.hasMany(db.Employee, {
+      foreignKey: 'departmentId',
+      onDelete: 'SET NULL'
+    })  
+    db.Employee.belongsTo(db.Department, {
+      foreignKey: 'departmentId'
+    })
 
-  await sequelize.sync()
+    await sequelize.sync()
+    console.log('Database sync completed')
+  } catch (error) {
+    console.error('Database connection error:', error)
+    throw error
+  }
 }
 
 initialize()
